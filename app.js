25588e455e3ffe7aca63bcc7978668c3d88be8ed
Jordan Kamin <Jordan@Jordans-MacBook-Air.local>
2014-06-23 10:47:17 -0700

    began MVC refactor

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  var controller = new Controller()
+  controller.bindListeners()
+});
+
--- script.js
+++ script.js
@@ -1,59 +1,68 @@
 $(document).ready(function () {
+  var userItem = $("#userInput").val();
+  $("#noString").hide();
+  $("#add").on("click", itemCleanUp);
 
-    $("#noString").hide();
-
-
-    $("#add").click(function () {
-        var userItem = $("#userInput").val();
-        if ($.trim(userItem) !== '') {
-            $('ul').append('<li>' + " " + userItem + '</li>');
-            $('input').val('');
-            $('<input>', {
-                type: "checkbox",
-            }).prependTo('li:last-child').addClass(".cb");
-            $("#noString").hide();
-        } else $("#noString").show();
-    });
-
-    $("#userInput").keydown(function(e){
-        if(e.which == 13) {
-        var userItem = $("#userInput").val();
-        if ($.trim(userItem) !== '') {
-            $('ul').append('<li>' + " " + userItem + '</li>');
-            $('input').val('');
-            $('<input>', {
-                type: "checkbox",
-            }).prependTo('li:last-child').addClass(".cb");
-            $("#noString").hide();
-        } else $("#noString").show();
+  $("#userInput").keydown(function(e){
+    if(e.which == 13) {
+      var userItem = $("#userInput").val();
+      if ($.trim(userItem) !== '') {
+        $('ul').append('<li>' + " " + userItem + '</li>');
+        $('input').val('');
+        $('<input>', {
+          type: "checkbox",
+        }).prependTo('li:last-child').addClass(".cb");
+        $("#noString").hide();
+      } else $("#noString").show();
     }
-    });
+  });
 
 
-    $("#clearChecked").click(function () {
-        $("li.bonus").remove();
-    });
-    
-    $("#clearAll").click(function () {
-        $("li").remove();
-    });
+//view//
+var View = function() {}
+
+View.prototype = {
+ itemCleanUp: function() {
+  debugger
+  if ($.trim(userItem) !== '') {
+      $('ul').append('<li>' + " " + userItem + '</li>');
+      $('input').val('');
+      $('<input>', {
+        type: "checkbox",
+      }).prependTo('li:last-child').addClass(".cb");
+      $("#noString").hide();
+    } else $("#noString").show();
+ }
+
+}
+
+
+
+
+  $("#clearChecked").click(function () {
+    $("li.bonus").remove();
+  });
+
+  $("#clearAll").click(function () {
+    $("li").remove();
+  });
 
 //checkbox functions for chekcing and unchecking
-        $('body').on('click', 'input:checkbox', function () {
-            $(this).parent("li").toggleClass("bonus");
+  $('body').on('click', 'input:checkbox', function () {
+    $(this).parent("li").toggleClass("bonus");
 
+  });
+
+  if ($('checkbox:checked').is('checked')) {
+    $('body').on('click', 'input:checkbox', function(){
+      $(this).parent("li").removeClass("bonus");
     });
-
-        if ($('checkbox:checked').is('checked')) {
-            $('body').on('click', 'input:checkbox', function(){
-            $(this).parent("li").removeClass("bonus");
-            });
-        }
+  }
 
 });
 
 
-    
+
 
 
 
